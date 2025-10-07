80a1b30baf56a3baeb8dd51e8821337f79eff8fa
Abhay Agrawal <abhayag41@gmail.com>
2025-10-07 23:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (54.11%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int balance = 0;
+        
+        for (char ch : s) {
+            if (ch == '(') {
+                if (balance > 0) {
+                    result += ch;
+                }
+                balance++;
+            } else {
+                balance--;
+                if (balance > 0) {
+                    result += ch;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
