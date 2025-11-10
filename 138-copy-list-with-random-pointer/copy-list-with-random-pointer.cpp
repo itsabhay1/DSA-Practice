cd5bbeeeb987ce156c383bda448f5ac9ce7a5942
Abhay Agrawal <abhayag41@gmail.com>
2025-11-11 02:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (86.55%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void insertCopyInBetween(Node* head) {
+        Node* temp = head;
+        while (temp != NULL) {
+            Node* nextElement = temp->next;
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = nextElement;
+            temp->next = copyNode;
+            temp = nextElement;
+        }
+    }
+    void connectRandomPointers(Node* head) {
+        Node* temp = head;
+        while (temp != NULL) {
+            Node* copyNode = temp->next;
+            if (temp->random) {
+                copyNode->random = temp->random->next;
+            } else {
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+    }
+    Node* getDeepCopyList(Node* head) {
+        Node* temp = head;
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        while(temp != NULL){
+            res->next = temp->next;
+            res = res->next;
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+    Node* copyRandomList(Node* head) {
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
+    }
+};
