7654c5be4bbe8a131d21e3137bef2618a255f75a
Abhay Agrawal <abhayag41@gmail.com>
2025-11-13 17:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.15%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        int cnt = 0;
+
+        while (n > 0) {
+            cnt += (n & 1);
+            n = n >> 1;
+        }
+
+        return cnt;
     }
 };
