818c6a025158df77b93f0411e1677ee4e56bb652
Abhay Agrawal <abhayag41@gmail.com>
2025-11-20 23:00:22 +0530

    Time: 1 ms (88.89%) | Memory: 21.8 MB (76.13%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        if (a[1] == b[1])
+            return a[0] > b[0];
+        else
+            return a[1] < b[1];
+    }
     int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b){
-            if(a[1] == b[1]) return a[0]>b[0];
-            else return a[1] < b[1];
-        });
-        int last1 = -1 , last2 = -1;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int last1 = -1, last2 = -1;
         int res = 0;
 
-        for(auto &it: intervals){
+        for (auto& it : intervals) {
             int s = it[0];
             int e = it[1];
 
-            bool last1_in = (last1>=s);
-            bool last2_in = (last2>=s);
+            bool last1_in = (last1 >= s);
+            bool last2_in = (last2 >= s);
 
-            if(last1_in && last2_in) continue;
-            if(!last1_in && !last2_in){
+            if (last1_in && last2_in)
+                continue;
+            if (!last1_in && !last2_in) {
                 last2 = e;
-                last1 = e-1;
+                last1 = e - 1;
                 res += 2;
-            }
-            else{
+            } else {
                 last1 = last2;
                 last2 = e;
                 res += 1;
