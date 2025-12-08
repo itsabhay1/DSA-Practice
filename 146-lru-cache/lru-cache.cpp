ad720d6df87560a4539e915e5a174a1c20ac8232
Abhay Agrawal <abhayag41@gmail.com>
2025-12-09 01:46:59 +0530

    Time: 52 ms (93.63%) | Memory: 173.2 MB (71.18%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,77 @@
+class LRUCache {
+public:
+    class Node {
+    public:
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+        Node(int key, int value) {
+            this->key = key;
+            this->value = value;
+            prev = nullptr;
+            next = nullptr;
+        }
+    };
+
+    unordered_map<int, Node*> mpp;
+    int capacity;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void deleteNode(Node* node){
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void insertAfterHead(Node* node){
+        Node* temp = head->next;
+        head->next = node;
+        node->next = temp;
+        temp->prev = node;
+        node->prev = head;
+    }
+
+    int get(int key) {
+        if(mpp.find(key) != mpp.end()){
+            Node* node = mpp[key];
+            deleteNode(node);
+            insertAfterHead(node);
+            return node->value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if(mpp.find(key) != mpp.end()){
+            Node* node = mpp[key];
+            node->value = value;
+            deleteNode(node);
+            insertAfterHead(node);
+        }
+        else{
+            if(mpp.size() == capacity){
+                Node* lru = tail->prev;
+                mpp.erase(lru->key);
+                deleteNode(lru);
+                delete lru;
+            }
+            Node* node = new Node(key, value);
+            mpp[key] = node;
+            insertAfterHead(node);
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
