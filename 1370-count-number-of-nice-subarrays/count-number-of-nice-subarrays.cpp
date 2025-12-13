ccdf2cba8324de7943bd9a0bb3286e0c8358307b
Abhay Agrawal <abhayag41@gmail.com>
2025-12-13 16:47:09 +0530

    Time: 15 ms (35.85%) | Memory: 71.3 MB (93.75%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int subarray(vector<int>& nums, int k) {
+        if(k<0) return 0;
         int l=0, r=0, cnt=0, odd=0;
         int n = nums.size();
         while(r<n){
