76b3acdd5c1a480bf6d961a5647043984215b04d
Abhay Agrawal <abhayag41@gmail.com>
2026-01-04 11:47:43 +0530

    Time: 42 ms (22.62%) | Memory: 47.8 MB (17.37%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>mp(26,0);
+        for(char &ch : tasks){
+            mp[ch-'A']++;
+        }
+
+        priority_queue<int>pq;
+        for(auto &p : mp){
+            if (p>0) pq.push(p);
+        }
+
+        int time = 0;
+        while(!pq.empty()){
+            vector<int>temp;
+            for(int i=1; i<=n+1; i++){
+                if(!pq.empty()){
+                   int freq = pq.top();
+                   pq.pop();
+                   freq--;
+                   temp.push_back(freq);
+                }
+            }
+
+            for(auto &f: temp){
+                if(f>0){
+                    pq.push(f);
+                }
+            }
+
+            if(pq.empty()){
+                time += temp.size();
+            }else{
+                time += n+1;
+            }
+        }
+        return time;
+
+    }
+};
