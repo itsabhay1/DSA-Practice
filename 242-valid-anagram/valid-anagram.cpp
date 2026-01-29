14304c0a7ed73a3d19aeff82730908320be43c1a
Abhay Agrawal <abhayag41@gmail.com>
2026-01-29 20:19:13 +0530

    Time: 1 ms (55.06%) | Memory: 9.5 MB (91.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int freq[26] = {0};
+        for(char ch: s){
+            freq[ch-'a']++;
+        }
+        for(char ch: t){
+            freq[ch-'a']--;
+        }
 
-        if(s == t) return true;
-        return false;
+        for(auto a: freq){
+            if(a != 0) return false;
+        }
+        return true;
     }
 };
