c591013660795e874d84403c9f116bbac166c472
Abhay Agrawal <abhayag41@gmail.com>
2026-01-31 01:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (54.61%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if (s.empty()) {
-            return 0;
-        }
-        
-        // Use standard library constants
-        const long long MAX_INT = INT_MAX;
-        const long long MIN_INT = INT_MIN;
-        
-        int i = 0;
-        int n = s.length();
-        
-        // Step 1: Skip leading whitespace
-        while (i < n && s[i] == ' ') {
-            i++;
-        }
-        
-        // Check if we've reached the end
-        if (i == n) {
-            return 0;
-        }
-        
-        // Step 2: Check for sign
+        long long ans = 0;
         int sign = 1;
-        if (s[i] == '+') {
-            i++;
-        } else if (s[i] == '-') {
-            sign = -1;
+        int i = 0;
+        int n = s.size();
+
+        while (i < n && s[i] == ' ') i++;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
             i++;
         }
-        
-        // Step 3: Read digits and convert
-        long long res = 0;
-        while (i < n && isdigit(s[i])) {
+        while (i < n && s[i] >= '0' && s[i] <= '9') {
             int digit = s[i] - '0';
-            res = res * 10 + digit;
-            
-            if (sign * res <= INT_MIN) {
-                return INT_MIN;
+            if (ans > (LLONG_MAX - digit) / 10) {
+                return sign == 1 ? INT_MAX : INT_MIN;
             }
-            if (sign * res >= INT_MAX) {
-                return INT_MAX;
-            }
-            
+            ans = ans * 10 + digit;
             i++;
         }
-        
-        // Step 4: Apply sign and return
-        return static_cast<int>(res * sign);     
+
+        ans *= sign;
+        if (ans > INT_MAX) return INT_MAX;
+        if (ans < INT_MIN) return INT_MIN;
+
+        return (int)ans;
     }
 };
