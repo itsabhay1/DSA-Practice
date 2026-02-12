4ac707f92c088a546f18888d3df02f0169c254bf
Abhay Agrawal <abhayag41@gmail.com>
2026-02-13 01:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.12%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long nn = n;
-        double ans = 1.0;
-        if(nn<0){
-            nn = -1*nn;
-            x = 1/x;
+        long long N = n;
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        while(nn>0){
-            if(nn % 2 == 1){
-                ans = ans*x;
-                nn = nn-1;
-            }
-            else{
-                x = x*x;
-                nn = nn/2;
-            }
 
-        }
-        return ans;
+        if (N == 0) return 1;
+
+        double half = myPow(x, N / 2);
+
+        if (N % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
     }
 };
