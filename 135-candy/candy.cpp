9af494e7e12633cc0357df80fff7b760cae1e935
Abhay Agrawal <abhayag41@gmail.com>
2026-02-19 18:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (53.39%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int sum = 0;
+        vector<int>arr(n,1);
+        for(int i=1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                arr[i] = arr[i-1]+1;
+            }
+        }
+        for(int i = n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                arr[i] = max(arr[i],arr[i+1]+1);
+            }
+        }
+
+        for(int i =0; i<n; i++){
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+};
