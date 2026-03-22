d632ee3f2c9a2aa934c161b2a310c7dcdd6402df
Abhay Agrawal <abhayag41@gmail.com>
2026-03-22 15:44:10 +0530

    Time: 463 ms (17.03%) | Memory: 8.7 MB (70.62%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,57 +1,37 @@
 class Solution {
 public:
-    vector<unordered_set<char>> rows, cols, boxes;
-
-    void solveSudoku(vector<vector<char>>& board) {
-        rows.resize(9);
-        cols.resize(9);
-        boxes.resize(9);
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-                    char c = board[i][j];
-                    rows[i].insert(c);
-                    cols[j].insert(c);
-                    boxes[(i/3)*3 + (j/3)].insert(c);
-                }
-            }
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c)
+                return false;
+            if (board[i][col] == c)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
         }
-        solve(board);
+        return true;
     }
 
-private:
     bool solve(vector<vector<char>>& board) {
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        int boxIndex = (i/3)*3 + (j/3);
-
-                        // Check if c is already used
-                        if(rows[i].count(c) == 0 &&
-                           cols[j].count(c) == 0 &&
-                           boxes[boxIndex].count(c) == 0){
-
-                            // Place the number
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
                             board[i][j] = c;
-                            rows[i].insert(c);
-                            cols[j].insert(c);
-                            boxes[boxIndex].insert(c);
 
-                            // Recurse
-                            if(solve(board)) return true;
-
-                            // Backtrack
-                            board[i][j] = '.';
-                            rows[i].erase(c);
-                            cols[j].erase(c);
-                            boxes[boxIndex].erase(c);
+                            if (solve(board) == true)
+                                return true;
+                            else
+                                board[i][j] = '.';
                         }
                     }
-                    return false; // No number worked here → backtrack
+                    return false;
                 }
             }
         }
         return true;
     }
+
+    void solveSudoku(vector<vector<char>>& board) { solve(board); }
 };
