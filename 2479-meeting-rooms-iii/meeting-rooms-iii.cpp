3a608c8526b5f7a0807bf37e2f11ca4e41bcc02c
Abhay Agrawal <abhayag41@gmail.com>
2026-04-19 12:52:54 +0530

    Time: 168 ms (15.82%) | Memory: 143.2 MB (18.46%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+
+        priority_queue<int, vector<int>, greater<int>> available;
+        for (int i = 0; i < n; i++) {
+            available.push(i);
+        };
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                       greater<>>
+            busy;
+        vector<int> count(n, 0);
+
+        for (auto m : meetings) {
+            long long st = m[0], end = m[1];
+            long long duration = end - st;
+
+            while (!busy.empty() && busy.top().first <= st) {
+                available.push(busy.top().second);
+                busy.pop();
+            }
+
+            if (!available.empty()) {
+                busy.push({end, available.top()});
+                count[available.top()]++;
+                available.pop();
+            } else {
+                auto [freeTime, room] = busy.top();
+                busy.pop();
+
+                long long newEnd = freeTime + duration;
+                busy.push({newEnd, room});
+                count[room]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (count[i] > count[ans])
+                ans = i;
+        }
+        return ans;
+    }
+};
