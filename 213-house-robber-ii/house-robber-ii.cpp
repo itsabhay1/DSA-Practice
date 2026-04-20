5a8bd5a1a4840285aa0edfbba9879b3482c51ba8
Abhay Agrawal <abhayag41@gmail.com>
2026-04-21 03:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (35.97%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, vector<int>& dp) {
-        dp[0] = nums[0];
-        int n = nums.size();
+    int solve(vector<int>& nums) {
+       int prev2 = 0;
+       int prev = nums[0];
+       int n = nums.size();
 
-        for (int i = 1; i < n; i++) {
-            int take = nums[i];
-            if (i > 1)
-                take += dp[i - 2];
-            int nottake = dp[i-1];
+       for(int i=1; i<n; i++){
+        int take = nums[i] + prev2;
+        int notTake = prev;
+        int curr = max(take, notTake);
 
-            dp[i] = max(take, nottake);
-        }
-        return dp[n-1];
+        prev2 = prev;
+        prev = curr;
+       }
+       return prev;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -25,9 +26,6 @@ public:
         // case 2 - removing first house
         vector<int> b(nums.begin() + 1, nums.end());
 
-        vector<int> dp1(a.size(), 0);
-        vector<int> dp2(b.size(), 0);
-
-        return max(solve(a, dp1), solve(b, dp2));
+        return max(solve(a), solve(b));
     }
 };
