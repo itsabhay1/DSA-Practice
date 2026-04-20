2a9bc6c335c94f0f5dd78aa71a39843905df1c5d
Abhay Agrawal <abhayag41@gmail.com>
2026-04-20 14:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,15 +3,15 @@ public:
     int climbStairs(int n) {
         if(n<=2) return n;
 
-        vector<int> dp(n+1);
-
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev1 = 2;
+        int prev2 = 1;
 
         for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
 
 
     }
