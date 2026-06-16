88f439653ff607d5ff06001189d78cc5b2a1b037
Abhay Agrawal <abhayag41@gmail.com>
2026-06-16 18:37:03 +0530

    Time: 11 ms (17.59%) | Memory: 39 MB (15.14%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,23 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_set<int> need;
+    bool dfs(TreeNode* root, int k, unordered_set<int>& need) {
+        if (!root) return false;
+
+        if (need.count(root->val)) return true;
+
+        need.insert(k - root->val);
+
+        return dfs(root->left, k, need) || dfs(root->right, k, need);
+    }
+
     bool findTarget(TreeNode* root, int k) {
-         if(root==NULL) return false;
-
-         if(need.count(root->val)) return true;
-
-         need.insert((k-root->val));
-         return findTarget(root->left, k) || findTarget(root->right, k);
+        unordered_set<int> need;
+        return dfs(root, k, need);
     }
 };
