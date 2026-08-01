d7dc9dc6f74bd98b376d244b0f325c9686a75595
Abhay Agrawal <abhayag41@gmail.com>
2026-08-01 13:55:38 +0530

    Time: 41 ms (59.61%) | Memory: 63.7 MB (54.64%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,44 +1,38 @@
 class Solution {
-private:
-    bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& graph, vector<int>& check){
-        vis[node] = 1;
-        pathVis[node] = 1;
-        check[node] = 0;
-
-        for(auto  it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,pathVis,graph,check)){
-                    return true;
-                }
-            }
-            else if(pathVis[it]){
-                return true;
-            }
-        }
-        check[node] = 1;
-        pathVis[node] = 0;
-        return false;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int v = graph.size();
-        vector<int> vis(v, 0);
-        vector<int> pathVis(v, 0);
-        vector<int> check(v, 0);
+        vector<int> adjRev[v];
+        vector<int> indegree(v, 0);
+
+        for(int i=0; i<v; i++){
+            for(auto &it: graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
         vector<int> safe;
 
         for(int i=0; i<v; i++){
-            if(!vis[i]){
-                dfs(i,vis,pathVis,graph,check);
-            }
+            if(indegree[i] == 0) q.push(i);
         }
 
-        for(int i=0; i<v; i++){
-            if(check[i] == 1){
-                safe.push_back(i);
-            }
-        }
+        while(!q.empty()){
+            int node = q.front();
+            safe.push_back(node);
+            q.pop();
 
+            for(auto &neigh: adjRev[node]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) q.push(neigh);
+            }
+            
+        }
+        sort(safe.begin(), safe.end());
         return safe;
+
+
     }
 };
