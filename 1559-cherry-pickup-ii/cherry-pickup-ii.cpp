f3f7a4ac5e6417fcd9a6e85ea816e72c580623aa
Abhay Agrawal <abhayag41@gmail.com>
2026-09-14 05:58:55 +0530

    Time: 59 ms (31.29%) | Memory: 13.4 MB (95.26%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -4,15 +4,15 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<vector<int>>> dp(
-            m, vector<vector<int>>(n, vector<int>(n, -1)));
+        vector<vector<int>> front(n, vector<int>(n ,0));
+        vector<vector<int>> curr(n, vector<int>(n ,0));
 
         for (int j1 = 0; j1 < n; j1++) {
             for (int j2 = 0; j2 < n; j2++) {
                 if (j1 == j2)
-                    dp[m - 1][j1][j2] = grid[m - 1][j1];
+                    front[j1][j2] = grid[m - 1][j1];
                 else
-                    dp[m - 1][j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
+                    front[j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
             }
         }
 
@@ -29,7 +29,7 @@ public:
                                 val = grid[i][j1] + grid[i][j2];
 
                             if (j1 + d1 >= 0 && j1 + d1 < n && j2 + d2 >= 0 && j2 + d2 < n)
-                                val += dp[i+1][j1+d1][j2+d2];
+                                val += front[j1+d1][j2+d2];
                             else
                                 val += -1e8;
 
@@ -37,11 +37,12 @@ public:
                         }
                     }
 
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            front = curr;
         }
 
-        return dp[0][0][n-1];
+        return front[0][n-1];
     }
 };
