9e86835499cc6f424c59f6ae8fe78fdf085a5404
Abhay Agrawal <abhayag41@gmail.com>
2026-09-15 16:45:42 +0530

    Time: 123 ms (62.85%) | Memory: 13.6 MB (81.44%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -9,21 +9,22 @@ public:
 
         int k = sum / 2;
 
-        vector<vector<bool>> dp(n, vector<bool>(k + 1, 0));
+        vector<bool> prev(k + 1, 0), curr(k+1,0);
 
-        for (int i = 0; i < n; i++) dp[i][0] = true;
-        if (nums[0] <= k) dp[0][nums[0]] = true;
+        prev[0] = curr[0] = true;
+        if (nums[0] <= k) prev[nums[0]] = true;
 
         for (int i = 1; i < n; i++) {
             for (int target = 1; target <= k; target++) {
-                bool np = dp[i - 1][target];
+                bool np = prev[target];
                 bool p = false;
-                if (target >= nums[i]) p = dp[i - 1][target - nums[i]];
+                if (target >= nums[i]) p = prev[target - nums[i]];
 
-                dp[i][target] = p || np;
+                curr[target] = p || np;
             }
+            prev = curr;
         }
 
-        return dp[n-1][k];
+        return prev[k];
     }
 };
